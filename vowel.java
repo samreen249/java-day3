b5093f9d9b2b5352e036da2a8dfb407ba9aa27c7
samreen249 <shaiksamreen1119@gmail.com>
2026-02-14 11:56:48 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class vowel {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        if(ch=='a'){
+            System.out.println("it  is vowel");
+        }
+        else if(ch=='e'){
+            System.out.println("it is vowel");
+        }
+        else if(ch=='i'){
+            System.out.println("it is vowel");
+        }
+        else if(ch=='o'){
+            System.out.println("it is vowel");
+        }
+        else if(ch=='u'){
+            System.out.println("it is vowel");
+        }
+        else{
+          System.out.println("it is not a vowel");  
+        }
+    }
+
+}
