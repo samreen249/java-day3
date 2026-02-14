dbc8e1a0393b96ea53655d5aa7221fb9aaf5bae2
samreen249 <shaiksamreen1119@gmail.com>
2026-02-14 14:59:26 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+       //int num=5;
+       int fact=1;
+       for(int i=1;i<=num;i++){
+        fact=fact*i;
+       } 
+       System.out.println(fact);
+    }
+    
+}
